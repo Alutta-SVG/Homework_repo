f4ca9ca4e9c52e491c65b01d25927a43742b1e2c
Alutta <Aluttamatambo5@gmail.com>
2024-09-08 17:50:47 +0200

    ameded comaprison operators

--- IWA_Challenges-main/Challenge 3/Challenge f/script.js
+++ IWA_Challenges-main/Challenge 3/Challenge f/script.js
@@ -1,22 +1,24 @@
 let date= 2050
-const status: 'student'
+const status ='student'
 let count = 0
 
-if (date = 2050) {
+if (date === 2050) {
 	console.log("January", 'New Year’s Day'
 	console.log("March", 'Human Rights Day')
+
 	date = 'April'
 	console.log(date, 'Family Day')
 	console.log(date, 'Freedom Day')
 	let count = count + 4
 
-	if (status = "student") {
+	if (status === "student") {
 	  console.log('June', 'Youth Day')
 		let count = count + 1
   }
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
+	
 	date = 'December'
 	console.log(date, 'Day of Reconciliation')
 	let count = count + 3
