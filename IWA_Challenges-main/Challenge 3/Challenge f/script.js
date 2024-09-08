0ea3a3cf8a8db199a973a4dbf4206c7506d076dd
Alutta <Aluttamatambo5@gmail.com>
2024-09-08 17:57:01 +0200

    amended parent

--- IWA_Challenges-main/Challenge 3/Challenge f/script.js
+++ IWA_Challenges-main/Challenge 3/Challenge f/script.js
@@ -23,7 +23,7 @@ if (date === 2050) {
 	console.log(date, 'Day of Reconciliation')
 	count = count + 3
 
-	if (status = "parent") {
+	if (status === "parent") {
 	  console.log(date, 'Christmas Day')
 	  count = count + 1
   }
