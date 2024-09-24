79fb5347692d02860a9390ccce64f0d06691ab7e
Alutta <Aluttamatambo5@gmail.com>
2024-09-24 11:16:25 +0200

    amended strings

--- IWA_Challenges-main/Challenge 5/Challenge i/script.js
+++ IWA_Challenges-main/Challenge 5/Challenge i/script.js
@@ -1,14 +1,14 @@
 const rent = 400;
-const tax = '8%';
+const tax = 0.08;
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = '00';
+const minuteOfDay = '00';
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
+if (hourOfDay && minuteOfDay !== null && hourOfDay == '00' && minuteOfDay == '00') {
 	const taxAsDecimal = tax / '100'
   const startingAfterTax = salary * '1' - taxAsDecimal
 	const balace = starting - transport - food - rent
