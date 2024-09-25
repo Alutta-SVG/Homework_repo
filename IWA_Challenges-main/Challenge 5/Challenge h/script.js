81b3bfded136c1a941b8ca1388807c13ae281950
Alutta <Aluttamatambo5@gmail.com>
2024-09-25 09:38:38 +0200

    Updated secondaryPhone

--- IWA_Challenges-main/Challenge 5/Challenge h/script.js
+++ IWA_Challenges-main/Challenge 5/Challenge h/script.js
@@ -4,7 +4,7 @@ const secondaryPhone = '0219131568'
 // Only change below this line
 
 const primaryValid = !Number.isNaN(parseInt(primaryPhone))
-const secondaryValid = typeof number == secondaryPhone
+const secondaryValid = !Number.isNaN(parseInt(secondaryPhone))
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
